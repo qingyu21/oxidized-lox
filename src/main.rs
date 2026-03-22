9dfe89ed55e1119f3eec285d2f63647a17803dd6
qingyu21 <853623491@qq.com>
2026-03-22 12:55:18 +0800

    feat: add CLI argument handling for script execution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io, process};
+
+fn main() -> io::Result<()> {
+    let args = env::args().skip(1).collect::<Vec<String>>();
+
+    match args.as_slice() {
+        [] => run_prompt()?,
+        [script] => run_file(script)?,
+        _ => {
+            eprintln!("Usage: oxidized-lox [script]");
+            process::exit(64);
+        }
+    }
+
+    Ok(())
+}
+
+fn run_file(path: &str) -> io::Result<()> {
+    println!("run file: {path}");
+    Ok(())
+}
+
+fn run_prompt() -> io::Result<()> {
+    println!("run prompt");
+    Ok(())
 }
