e0ad7b1878d315bb0a392ef6172d7829e728183e
qingyu21 <853623491@qq.com>
2026-04-07 18:25:19 +0800

    chore: derive RuntimeError with thiserror

--- src/runtime/error.rs
+++ src/runtime/error.rs
@@ -1,6 +1,9 @@
+use thiserror::Error;
+
 use crate::token::Token;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Error)]
+#[error("{message}")]
 pub(crate) struct RuntimeError {
     pub(crate) token: Token,
     pub(crate) message: String,
